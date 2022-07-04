635d60993ec05bc984e4d319a1aa31cd6870d35d
Ogishi Gen <77260650+oogishigen@users.noreply.github.com>
2022-07-04 14:59:34 +0900

    Update script.js

--- deadline-driven-timer/script.js
+++ deadline-driven-timer/script.js
@@ -1,24 +1,17 @@
 function updateUIOfCountdown() {
-
-   window.addEventListener('load', function () {
-      updateUIOfCountdown();
-      setInterval(updateUIOfCountdown, 1000);
-   })
-
    const now = new Date(); //現在時刻を取得
 
    location.search.substring(1 + "deadline=", location.search.length);
    const v = location.search.split("=")[1];
    const par = parseInt(v);
 
-
    const time = new Date(par);
 
-   const diff = time.getTime() - now.getTime(); //時間の差を取得（ミリ秒）
+   const diff = time.getTime() - now.getTime() - 32400000; //時間の差を取得（ミリ秒）取得した時間は日本時間と9時間の差がある。9時間をmsに変換したものを引いた。
 
    //ミリ秒から単位を修正
    const calcDay = Math.floor(diff / 1000 / 60 / 60 / 24);
-   const calcHour = Math.floor(diff % (24 * 60 * 60 * 1000) / 1000 / 60 / 60) - 9;
+   const calcHour = Math.floor(diff % (24 * 60 * 60 * 1000) / 1000 / 60 / 60);
    const calcMin = Math.floor(diff % (24 * 60 * 60 * 1000) / 1000 / 60) % 60;
    const calcSec = Math.floor(diff % (24 * 60 * 60 * 1000) / 1000) % 60;
 
@@ -32,11 +25,10 @@ function updateUIOfCountdown() {
    hour.innerHTML = calcHour < 10 ? '0' + calcHour : calcHour;
    min.innerHTML = calcMin < 10 ? '0' + calcMin : calcMin;
    sec.innerHTML = calcSec < 10 ? '0' + calcSec : calcSec;
-}
 
+}
 
 window.addEventListener('load', function () {
     updateUIOfCountdown();
     setInterval(updateUIOfCountdown, 1000);
 })
-
